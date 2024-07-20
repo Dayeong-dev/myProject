71252dc8a107cc1b257269bdbd84c1f5d7352279
Yun Dayeong <ydiosa98@gmail.com>
2024-07-20 23:43:35 +0900

    First commit

---
+++ js/main.js
@@ -0,0 +1,65 @@
+// window.addEventListener("load", () => {
+//     const grid = new Isotope("section", {
+//         itemSelector: "article",
+//         columnWidth: "article",
+//         transitionDuration: "0.5s"
+//     });
+    
+//     const btns = document.querySelectorAll("main ul li");
+
+//     for(let el of btns) {
+//         el.addEventListener("click", e => {
+//             e.preventDefault();
+//             const sort = e.currentTarget.querySelector("a").getAttribute("href");
+
+//             grid.arrange({
+//                 filter: sort
+//             });
+
+//             for(let el of btns) {
+//                 el.classList.remove("on");
+//             }
+
+//             e.currentTarget.classList.add("on");
+//         });
+//     }
+// });
+
+const frame = "section";
+const box = "article";
+const speed = "0.5s";
+const activeClass = "on";
+const btns = document.querySelectorAll("main ul li");
+let grid;
+
+window.addEventListener("load", e => {
+    init();
+    filter(btns);
+});
+
+function init() {
+    grid = new Isotope(frame, {
+        itemSelector: box,
+        columnWidth: box,
+        transitionDuration: speed
+    });
+}
+
+function filter(arr) {
+    for(let el of arr) {
+        el.addEventListener("click", e => {
+            e.preventDefault();
+            const sort = e.currentTarget.querySelector("a").getAttribute("href");
+
+            grid.arrange({
+                filter: sort
+            });
+
+            for(let el of arr) {
+                el.classList.remove(activeClass);
+            }
+
+            e.currentTarget.classList.add(activeClass);
+        });
+    }
+}
